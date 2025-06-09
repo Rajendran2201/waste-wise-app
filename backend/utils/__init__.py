154f6d6de31e95b7495e280f4351c802363d335b
Raj <asrajendrayadav@gmail.com>
2025-06-09 19:32:59 +0530

    Add TryExcept module to utils __init__.py for YOLOv5 compatibility

---
+++ backend/utils/__init__.py
@@ -0,0 +1,8 @@
+# Add TryExcept module for YOLOv5 compatibility
+class TryExcept:
+    """Try-Except wrapper for YOLOv5 compatibility"""
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def __call__(self, x):
+        return x
